8b5a4d144770abf1ae5f1b6945a599a4e0f87782
Hatim Lahwaouir <hlahwaou@e1r2p6.1337.ma>
2023-11-01 22:48:46 +0100

    last version

--- cpp06/ex02/Base.cpp
+++ cpp06/ex02/Base.cpp
@@ -43,21 +43,21 @@ void identify(Base& p)
 {
     try
     {
-        dynamic_cast<B &>(p);
+        (void)dynamic_cast<B &>(p);
         std::cout << "B" << std::endl;
         return;
     }
     catch(const std::exception& e){ }
        try
     {
-        dynamic_cast<A &>(p);
+        (void)dynamic_cast<A &>(p);
         std::cout << "A" << std::endl;
         return;
     }
     catch(const std::exception& e){ } 
     try
     {
-        dynamic_cast<C &>(p);
+        (void)dynamic_cast<C &>(p);
         std::cout << "C" << std::endl;
         return;
     }
